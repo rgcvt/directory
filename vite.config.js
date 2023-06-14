dbaec6397a32c49b102661d6bba70eac2009171a
Andrew Hale <services.hale.adh@gmail.com>
2023-06-13 20:59:44 -0400

    pwa-ify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
 		'process.env': {},
 	},
 	plugins: [
+		VitePWA(),
 		vue({
 			reactivityTransform: true,
 		}),
